c91a868bc92dc0977df261f984c382b1d334fd4b
Linus <linus-tiemann@live.de>
2021-01-25 11:24:25 +0100

    added first movement test, VRIK + Spare Movement

---
+++ Assets/Internal/MovementInput.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MovementInput : MonoBehaviour
+{
+    public bool DisableSideWayMovement;
+    
+    public SteamVR_ActionSet actionSetEnable;
+    public SteamVR_Action_Vector2 Input;
+
+    private NavMeshMovement Mover;
+    
+
+    private bool UseIsPressed; 
+    
+    private bool InputAllowed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputAllowed = true;
+        actionSetEnable.Activate();
+        Mover = GetComponent<NavMeshMovement>();
+
+        // Use.AddOnActiveChangeListener,SteamVR_Input_Sources.Any);
+        
+        //Use.AddOnStateDownListener(TriggerUp, SteamVR_Input_Sources.Any);
+        UseIsPressed = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 Movement = Input.GetAxis(SteamVR_Input_Sources.Any);
+
+        if (DisableSideWayMovement)
+        {
+            Movement.x = 0;
+        }
+        
+        if(InputAllowed)
+            Mover.Move(Movement);
+        
+    }
+    
+
+
+    public void AllowInput(bool state)
+    {
+        InputAllowed= state;
+    }
+
+   
+
+    
+
+    private void SetUseState(bool state=true)
+    {
+        UseIsPressed = state;
+    }
+}
---
+++ Assets/Internal/NavMeshMovement.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class NavMeshMovement : MonoBehaviour
+{
+    [SerializeField] private Transform  _hmdTransform;
+    [SerializeField] private GameObject  head;
+    [SerializeField] private GameObject  Orientation;
+    private NavMeshAgent agent;
+    [Range(0.0f,15.0f)] public float speed;
+    private Vector3 BodyDirection;
+    private bool freezed;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.updateRotation = true;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(!freezed)
+            head.transform.position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Quaternion targetRot = Quaternion.LookRotation(Orientation.transform.forward);
+        Vector3 eulerRotation = new Vector3();
+        
+        eulerRotation= Vector3.ProjectOnPlane(targetRot.eulerAngles, Vector3.forward);
+        eulerRotation.x = 0f;
+        targetRot = Quaternion.Euler(eulerRotation);
+
+
+        this.transform.rotation = targetRot;
+        
+        
+        agent.angularSpeed = 300;
+
+        
+        
+    }
+
+    public void Move(Vector2 direction)
+    {
+        BodyDirection = new Vector3( direction.x,0f,direction.y  );
+        BodyDirection =  this.transform.rotation* BodyDirection;
+        //Debug.Log("body" + BodyDirection);
+        //Debug.Log("head" + direction);
+        MoveInToDirection(BodyDirection);
+    }
+
+    private void MoveInToDirection(Vector3 direction)
+    {
+        
+        agent.Move(Time.deltaTime*speed*direction);
+       
+    }
+
+
+    private void FreezeMover()
+    {
+       
+    }
+    
+    
+    public void SetToPositionAndRotation(Vector3 pos, Quaternion rot)
+    {
+        // todo set rotation as well
+
+       FreezeMover();
+        transform.position = pos;
+        agent.Warp(pos);
+
+        transform.rotation = rot;
+
+        Valve.VR.OpenVR.Compositor.SetTrackingSpace(
+            Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
+
+        
+        
+        //agent.enabled = true;
+    }
+    
+    
+}
