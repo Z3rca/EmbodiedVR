bf6e3c3591d6b79805d3a5521ae7dcb99e422a91
Linus <linus-tiemann@live.de>
2021-03-07 19:26:02 +0100

    I broked VR movement, had it working

---
+++ Assets/Scripts/Movement/Animation/SimpleLocomotionAnimationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleLocomotionAnimationController: MonoBehaviour
+{
+    private CharacterController characterController;
+
+    private InputData _inputData;
+    public MovementInput input;
+
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = input.GetComponent<CharacterController>();
+        input.InputEvent += ReadInputAsAnimation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = camera.transform.position;
+
+    }
+
+
+    private void ReadInputAsAnimation(object sender, InputData inputData)
+    {
+        Debug.Log("received input " + inputData.input );
+        if (inputData.input != Vector2.zero)
+        {
+            GetComponent<Animator>().SetBool("IsMoving",true);
+            GetComponent<Animator>().SetFloat("X", inputData.input.x);
+            GetComponent<Animator>().SetFloat("Z", inputData.input.y);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("IsMoving",false);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Input/InputEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputData: EventArgs
+{
+   public Vector2 input;
+   public double timeStamp;
+}
