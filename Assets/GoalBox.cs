47eb5da0d8f369ee5ae030517483d9139cab703f
Henrik Himmelreich <hepahi@web.de>
2021-05-04 16:08:05 +0200

    Started working on goal box in area 2

---
+++ Assets/GoalBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBox : MonoBehaviour
+{
+
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Sphere")
+        {
+            counter++;
+        }
+    }
+}
