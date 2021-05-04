472ff19e21d675ebbfaa7b4e6b0d02b2c34111e5
Linus <linus-tiemann@live.de>
2021-05-04 21:58:53 +0200

    rudimentary elevator

--- Assets/Scripts/Enivironment/Elevator.cs
+++ Assets/Scripts/Enivironment/Elevator.cs
@@ -5,55 +5,47 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    
+    public RemoteControl remoteControl;
     [SerializeField] private GameObject elevator;
     [SerializeField] private GameObject UpperBoundary;
     [SerializeField] private GameObject LowerBoundary;
 
     [SerializeField] private float Speed;
 
-    private float posY;
+    private float _inputX;
+    private float _inputY;
     // Start is called before the first frame update
     void Start()
     {
+        remoteControl.OnInputReceiver += Move;
+    }
+    
+
+
+    void Update()
+    {
+        
     }
 
     // Update is called once per frame
 
 
-    
-
-
-    private void FixedUpdate()
+    private void Move(object sender, InputArgs input)
     {
+        _inputX = input.inputValue.x;
+        _inputY = input.inputValue.y;
         
-        
-        
-        if (Input.GetKey(KeyCode.B))
-        {
-            var position = elevator.transform.position;
-            float input = 1f;
-            posY = position.y;
-            float posX = position.x;
-            float posZ = position.z;
-            posY += (input * Speed * Time.deltaTime);
-            posY =  Mathf.Clamp(posY,  
-                (LowerBoundary.transform.position.y), 
-                (UpperBoundary.transform.position.y));
-            elevator.transform.position = new Vector3(posX, posY, posZ);;
-        }
-        
-        if (Input.GetKey(KeyCode.C))
-        {
-            var position = elevator.transform.position;
-            float input = -1;
-            posY = position.y;
-            float posX = position.x;
-            float posZ = position.z;
-            posY += (input * Speed * Time.deltaTime);
-            posY =  Mathf.Clamp(posY,  
-                (LowerBoundary.transform.position.y), 
-                (UpperBoundary.transform.position.y));
-            elevator.transform.position = new Vector3(posX, posY, posZ);;
-        }
+        var position = elevator.transform.position;
+        float posY = position.y;
+        float posX = position.x;
+        float posZ = position.z;
+        posY += (_inputY * Speed * Time.deltaTime);
+        posY =  Mathf.Clamp(posY,  
+            (LowerBoundary.transform.position.y), 
+            (UpperBoundary.transform.position.y));
+        elevator.transform.position = new Vector3(posX, posY, posZ);;
     }
+
+    
 }
---
+++ Assets/Scripts/Enivironment/RemoteControl.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RootMotion;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class RemoteControl : MonoBehaviour
+{
+    
+    
+    public  SteamVR_ActionSet LiftActionset;
+    [SerializeField] private GameObject Joystick;
+    
+    private SteamVR_Action_Vector2 MovementInput;
+
+    public UnityEvent<Vector2> OutputEvent;
+    
+    private Vector2 movementInput;
+
+    public bool AllowInputX;
+    public bool AllowInputY;
+
+
+    private float InputValue;
+
+    private bool activated;
+    private Hand hand;
+    
+    
+    public event EventHandler<InputArgs> OnInputReceiver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MovementInput = SteamVR_Input.GetAction<SteamVR_Action_Vector2>(LiftActionset.GetShortName(), "Direction");
+    }
+
+
+    public void ActivateActionSet(bool state)
+    {
+        Debug.Log("is now on" + state);
+        if (state == true)
+        {
+            LiftActionset.Activate();
+            activated = true;
+            hand = this.gameObject.GetComponent<Interactable>().attachedToHand;
+            
+        }
+        else
+        {
+            activated = false;
+            LiftActionset.Deactivate();
+        }
+        
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            
+            movementInput = MovementInput.axis;
+            Vector3  resultMovement= new Vector3();
+            if (AllowInputY)
+            {
+                resultMovement += new Vector3(0, 0, movementInput.y);
+            }
+            else
+            {
+                movementInput.y = 0;
+            }
+            if (AllowInputX)
+            {
+                resultMovement += new Vector3(movementInput.x, 0,0 );
+            }
+            else
+            {
+                movementInput.x = 0;
+            }
+            
+            Joystick.transform.localPosition = resultMovement*0.015f;
+
+            InputArgs args = new InputArgs();
+
+            args.inputValue = movementInput;
+
+
+            OnInputReceiver?.Invoke(this, args);
+            
+        }
+    }
+
+    
+}
+
+
+
+public class InputArgs : EventArgs
+{
+    public Vector2 inputValue;
+}
