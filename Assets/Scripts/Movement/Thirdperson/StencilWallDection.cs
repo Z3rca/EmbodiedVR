83c1ec6c3550dab15facb93679a74513cb7c6049
Linus <linus-tiemann@live.de>
2021-05-06 15:37:52 +0200

    added stencil to Thirdperson Prefab
    overrides to much currently, need fix

---
+++ Assets/Scripts/Movement/Thirdperson/StencilWallDection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StencilWallDection : MonoBehaviour
+{
+    private Material tmp_Material;
+    
+    private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
