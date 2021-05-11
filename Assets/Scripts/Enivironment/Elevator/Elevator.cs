3614a34cdfdd2cacb7f9d6f3df02292ac8e07264
Linus <linus-tiemann@live.de>
2021-05-11 17:02:25 +0200

    fixed movement of the elevator
    avatar is still wiggle

--- Assets/Scripts/Enivironment/Elevator/Elevator.cs
+++ Assets/Scripts/Enivironment/Elevator/Elevator.cs
@@ -28,12 +28,17 @@ public class Elevator : MonoBehaviour
 
     void Update()
     {
-        elevator.transform.position = targetPos; 
+        
     }
 
     private void FixedUpdate()
     {
         
+    }
+
+    private void LateUpdate()
+    {
+        elevator.transform.position = targetPos; 
     }
 
     // Update is called once per frame
