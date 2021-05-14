d7e9ecc75402830cc6ac8def7331e90002044955
Henrik Himmelreich <hepahi@web.de>
2021-05-14 13:59:52 +0200

    Added timer that opens door in area 2

---
+++ Assets/Door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public bool isLeftDoor = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void openDoor()
+    {
+        if (isLeftDoor)
+        {
+            // open to left side
+            gameObject.transform.Translate(-1,0,0);
+        }
+        else
+        {
+            //open to right side
+            gameObject.transform.Translate(1,0,0);
+        }
+    }
+}
