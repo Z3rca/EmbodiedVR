aba6200544c04ba34c662acb49268bf8c6103e9e
Linus <linus-tiemann@live.de>
2021-06-27 15:18:59 +0200

    Added spawner script

---
+++ Assets/Scripts/ExperimentManager/ExperimentManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ExperimentManager : MonoBehaviour
+{
+    public static ExperimentManager Instance { get; private set; }
+    
+    
+    public GameObject Player;
+
+    public GameObject ActiveStation;
+    public List<StationSpawner> RemainingstationsStationSpawners;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeParticipantToNextStation()
+    {
+        Player.GetComponent<PhysicalMovement>().TeleportToPosition(Remainingstations[Remainingstations.Count].transform.position);
+    }
+
+
+    public void RegisterSpawnerToList(StationSpawner spawner)
+    {
+        RemainingstationsStationSpawners.Add(spawner);
+    }
+}
---
+++ Assets/Scripts/ExperimentManager/StationSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationSpawner : MonoBehaviour
+{
+    public string ID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExperimentManager.Instance.G
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    
+}
