cdf5ee3574c245c11be97495ba34c30fd09c2626
Linus <linus-tiemann@live.de>
2021-06-28 23:00:27 +0200

    closing door after passing through data gathering collider

---
+++ Assets/Scripts/ExperimentManager/CloseDoorTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CloseDoorTrigger : MonoBehaviour
+{
+    private ExperimentManager experimentManager;
+    
+    private PhysicalMovement playerBody;
+
+    public UnityEvent OnExitTrigger;
+
+
+    private void Start()
+    {
+        if (ExperimentManager.Instance != null)
+        {
+            experimentManager = ExperimentManager.Instance;
+            playerBody = experimentManager.Player.GetComponentInChildren<PhysicalMovement>();
+        }
+        else
+        {
+            Debug.Log(this.gameObject.name + " was unable to assign player. wont work");
+        }
+
+
+        if (playerBody == null)
+        {
+            Debug.Log("Player body couln't be assigned");
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == playerBody.gameObject)
+        {
+            OnExitTrigger.Invoke();   
+        }
+    }
+}
---
+++ Assets/Scripts/ExperimentManager/DoorControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public GameObject Door;
+    public GameObject Door2;
+
+
+    public void CloseDoor()
+    {
+        Door.gameObject.SetActive(true);
+        Door2.gameObject.SetActive(true);
+    }
+    
+    
+    public void OpenDoor()
+    {
+        Door.gameObject.SetActive(true);
+        Door2.gameObject.SetActive(true);
+    }
+}
