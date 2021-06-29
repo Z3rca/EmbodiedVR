6d33ec28622ed3cc94986adf70efc0a807e787b1
Henrik Himmelreich <hepahi@web.de>
2021-06-29 15:50:54 +0200

    Worked on expanding rating board to include all measurements

---
+++ Assets/MeasuringFlow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasuringFlow : MonoBehaviour
+{
+    public GameObject audioMeasuringTool;
+    public GameObject motionSicknessMeasuringTool;
+    public GameObject posturalStabilityMeasuringTool;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void measureAudio()
+    {
+        audioMeasuringTool.SetActive(true);
+    }
+    
+    void measureMotionSickness()
+    {
+        motionSicknessMeasuringTool.SetActive(true);
+    }
+
+    void measurePosturalStability()
+    {
+        posturalStabilityMeasuringTool.SetActive(true);
+    }
+}
