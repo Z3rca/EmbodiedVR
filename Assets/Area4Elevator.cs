73362fae5c37e4f09f047137c6613619bca91aa8
Henrik Himmelreich <hepahi@web.de>
2021-07-27 11:49:24 +0200

    Implemented first elevator in area 4

---
+++ Assets/Area4Elevator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Area4Elevator : MonoBehaviour
+{
+
+    public GameObject elevator;
+
+    public LinearMapping linearMapping;
+    
+    public float upperBoundary;
+    public float lowerBoundary;
+
+    public float speed;
+
+    private float posX;
+    private float posZ;
+    
+    private float currentLinearMapping = float.NaN;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        posX = elevator.transform.position.x;
+        posZ = elevator.transform.position.z;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentLinearMapping != linearMapping.value)
+        {
+            currentLinearMapping = linearMapping.value;
+        }
+
+        if (currentLinearMapping > 0.95)
+        {
+            MoveUpwards();
+        }
+        
+        if (currentLinearMapping < 0.05)
+        {
+            MoveDownwards();
+        }
+    }
+
+    public void MoveUpwards()
+    {
+        if (elevator.transform.position.y <= upperBoundary)
+        {
+            float posY = elevator.transform.position.y;
+            
+            posY += speed * Time.deltaTime;
+
+            elevator.transform.position = new Vector3(posX, posY, posZ);
+        }
+    }
+    
+    public void MoveDownwards()
+    {
+        if (elevator.transform.position.y >= lowerBoundary)
+        {
+            float posY = elevator.transform.position.y;
+            
+            posY -= speed * Time.deltaTime;
+
+            elevator.transform.position = new Vector3(posX, posY, posZ);
+        }
+    }
+}
