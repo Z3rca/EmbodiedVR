837ac256fa910bae09b6a875d3f65465e5086e79
Linus <linus-tiemann@live.de>
2021-08-21 08:46:52 +0200

    relevant Area changes for DataSaving

---
+++ Assets/Scripts/DataSaving/ExperimentRelevant/MotionsicknessRatingLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionsicknessRatingLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExperimentManager.Instance.startExperiment += OnStartExperiment;
+    }
+
+
+    void OnStartExperiment(object sender, StartExperimentArgs startExperimentArgs)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
