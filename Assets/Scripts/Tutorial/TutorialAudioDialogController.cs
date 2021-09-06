474463d9da9c0742374f41466d3057968499f8f5
Henrik Himmelreich <hepahi@web.de>
2021-09-06 14:52:10 +0200

    Updated Tutorial Audio Controller with new audio clips

--- Assets/Scripts/Tutorial/TutorialAudioDialogController.cs
+++ Assets/Scripts/Tutorial/TutorialAudioDialogController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class TutorialAudioDialogController : MonoBehaviour
 {
-   public AudioClip Familarization;
-   public AudioClip SwitchViewButton;
-   public AudioClip Movement;
-   public AudioClip Interaction;
-   public AudioClip PickUp;
-   public AudioClip ThrowBallInBoxInstruction;
-   public AudioClip ThrowBallInBox;
-   public AudioClip ExitTutorial;
+   public AudioClip clip_1;
+   public AudioClip clip_2;
+   public AudioClip clip_3;
+   public AudioClip clip_4;
+   public AudioClip clip_5;
+   public AudioClip clip_6;
+   public AudioClip clip_7;
+   public AudioClip clip_8;
+   public AudioClip clip_9;
+   public AudioClip clip_10;
+   public AudioClip clip_11;
+   public AudioClip clip_12;
+   public AudioClip clip_13;
    public AudioSource audioSource;
 
    private bool _playingAudioClip;
@@ -22,44 +27,69 @@ public class TutorialAudioDialogController : MonoBehaviour
    }
 
 
-   public void FamilarizationAudioClip()
+   public void AudioClip1()
    {
-      StartAudioClip(Familarization);
+      StartAudioClip(clip_1);
    }
 
-   public void SwitchViewButtonAudioClip()
+   public void AudioClip2()
    {
-      StartAudioClip(SwitchViewButton);
+      StartAudioClip(clip_2);
    }
    
-   public void MovementAudioClip()
+   public void AudioClip3()
    {
-      StartAudioClip(Movement);
+      StartAudioClip(clip_3);
    }
 
-   public void InteractionAudioClip()
+   public void AudioClip4()
    {
-      StartAudioClip(Interaction);
+      StartAudioClip(clip_4);
    }
    
-   public void PickBallAudioClip()
+   public void AudioClip5()
    {
-      StartAudioClip(PickUp);
+      StartAudioClip(clip_5);
    }
    
-   public void ThrowBallInBoxInstructionAudioClip()
+   public void AudioClip6()
    {
-      StartAudioClip(ThrowBallInBoxInstruction);
+      StartAudioClip(clip_6);
    }
    
-   public void ThrowBallInBoxAudioClip()
+   public void AudioClip7()
    {
-      StartAudioClip(ThrowBallInBox);
+      StartAudioClip(clip_7);
    }
 
-   public void ExitTutorialAudioClip()
+   public void AudioClip8()
    {
-      StartAudioClip(ExitTutorial);
+      StartAudioClip(clip_8);
+   }
+   
+   public void AudioClip9()
+   {
+      StartAudioClip(clip_9);
+   }
+   
+   public void AudioClip10()
+   {
+      StartAudioClip(clip_10);
+   }
+   
+   public void AudioClip11()
+   {
+      StartAudioClip(clip_11);
+   }
+   
+   public void AudioClip12()
+   {
+      StartAudioClip(clip_12);
+   }
+   
+   public void AudioClip13()
+   {
+      StartAudioClip(clip_13);
    }
    
    public void FinishedTask()
