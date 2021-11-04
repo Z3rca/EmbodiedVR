28feaf813766b9951ac41eb8777950766be233fb
Linus <linus-tiemann@live.de>
2021-11-04 20:49:05 +0100

    reworked shutter

--- Assets/Scripts/Scene Functionality/Shutter.cs
+++ Assets/Scripts/Scene Functionality/Shutter.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shutter : MonoBehaviour
 {
-
+    public GameObject shutterOrigin;
+    public GameObject UpperPosition;
+    public GameObject LowerPosition;
     public float speed = 1;
     bool activated = false;
-    private string direction = "up";
-
+    private bool directionUp;
+    
+    private bool _switch;
+    private bool _recovering;
+    
     // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        shutterOrigin.transform.position = LowerPosition.transform.position;
     }
 
     // Update is called once per frame
@@ -22,30 +29,55 @@ public class Shutter : MonoBehaviour
         {
           //  Debug.Log("Activated");
             // if position has reached top or bottom flip direction
-            if (transform.position.y > 2)
+            if (shutterOrigin.transform.position.y>=UpperPosition.transform.position.y)
             {
-                direction = "down";
-          //      Debug.Log("Should move down now");
+                directionUp = false;
+                //      Debug.Log("Should move down now");
             }
-            else if (transform.position.y < 1.5)
+            
+            if (shutterOrigin.transform.position.y<=LowerPosition.transform.position.y)
             {
-                direction = "up";
+                directionUp = true;
             }
 
             // move shutter in direction defined
-            if (direction == "up")
+            if (directionUp)
             {
-                transform.Translate(Vector3.up * (Time.deltaTime * speed), Space.World);
+                shutterOrigin.transform.Translate(Vector3.up * (Time.deltaTime * speed), Space.World);
             }
-            else if (direction == "down")
+            else if (!directionUp)
             {
-                transform.Translate(Vector3.down * (Time.deltaTime * speed), Space.World);
+                shutterOrigin.transform.Translate(Vector3.down * (Time.deltaTime * speed), Space.World);
             }
         }
     }
+    
+    
+    
 
-    public void Movement()
+    public void StartMoving(bool state)
     {
-        activated = ! activated;
+        activated = state;
+    }
+
+    public void SwitchMoving()
+    {
+        if (_recovering)
+            return;
+        _recovering = true;
+        _switch = !_switch;
+        StartMoving(_switch);
+    }
+
+    public void ButtonRecovery()
+    {
+        StartCoroutine(DelayedRecovery(2f));
+    }
+    private IEnumerator DelayedRecovery(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        _recovering = false;
+
     }
 }
