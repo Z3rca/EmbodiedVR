55d01a690192b7bdf76074cc8c5b48897ed53a07
Linus <linus-tiemann@live.de>
2021-11-04 23:48:03 +0100

    fixed elevator logic and mesh

---
+++ Assets/Scripts/Enivironment/Elevator/ElevatorCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorCheck : MonoBehaviour
+{
+    [SerializeField] private bool _isUpstairs;
+    private bool _isInside;
+    
+    private GameObject avatar;
+    public bool IsAvatarIsInside()
+    {
+        return _isInside;
+    }
+
+    public bool isUpstairs()
+    {
+        return _isUpstairs;
+    }
+
+    public Vector3 AvatarPosition()
+    {
+        return avatar.transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterController>())
+        {
+            _isInside = true;
+            avatar = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<CharacterController>())
+        {
+            _isInside = false;
+        }
+    }
+}
